c0d1b527d04ea27fd4fffd27ad37f9593748ac89
cedric arnaud <79386450+CedricArnaud20@users.noreply.github.com>
2024-11-20 19:24:35 -0300

    broswe test k6

---
+++ BrowserTesTk6/WebTest.js
@@ -0,0 +1,40 @@
+import { browser } from 'k6/browser';
+
+export const options = {
+    scenarios: {
+        ui: {
+            executor: 'shared-iterations',
+            options: {
+                browser: {
+                    type: 'chromium',
+                },
+            },
+        },
+    },
+    thresholds: {
+        checks: ['rate==1.0'],
+        browser_web_vital_lcp: ['p(90) < 1000'],
+        
+    },
+};
+
+export default async function () {
+    const page = await browser.newPage();
+
+    try {
+        await page.goto('https://buger-eats.vercel.app/');
+        await page.click('a[href="/deliver"]');
+
+        const header = await page.$('h1')
+        const headerText = await header.textContent();
+
+        if (headerText.includes('Cadastre-se para ')) {
+            console.log('Texto encontrado: ', headerText);
+        } else {
+            console.error('Texto não encontrado');
+        }    
+
+        } finally {
+            await page.close();
+        }
+    }
